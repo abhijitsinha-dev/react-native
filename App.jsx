00226c78e1c246daa39dafd03131b9a5c8aaaa6d
Abhijit Sinha <abhijitsinhaofficial@gmail.com>
2025-05-30 01:11:37 +0530

    Bottom Tab Navigation Complete

--- App.jsx
+++ App.jsx
@@ -1,37 +1,46 @@
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Home from './src/navigation/Home';
 import About from './src/navigation/About';
 import Contact from './src/navigation/Contact';
 
-const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const setTabIcon = (routeName, focused, color, size) => {
+  let iconname;
+
+  if (routeName === 'Home') {
+    iconname = focused ? 'home' : 'home-outline';
+  } else if (routeName === 'About') {
+    iconname = focused ? 'information-circle' : 'information-circle-outline';
+  } else if (routeName === 'Contact') {
+    iconname = focused ? 'mail' : 'mail-outline';
+  }
+
+  return <Icon name={iconname} color={color} size={size} />;
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
+      <Tab.Navigator
+        screenOptions={({route}) => ({
           headerTitle: '',
-          headerTintColor: 'white',
           headerTransparent: true,
-          drawerStyle: {
-            backgroundColor: '#16a085',
-          },
-          drawerLabelStyle: {
-            color: 'white',
-            textAlign: 'center',
-          },
-          drawerActiveTintColor: 'black',
-          drawerPosition: 'right',
-          sceneStyle: {
-            backgroundColor: '#1abc9c',
-          },
-        }}>
-        <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Screen name="About" component={About} />
-        <Drawer.Screen name="Contact" component={Contact} />
-      </Drawer.Navigator>
+          tabBarInactiveBackgroundColor: '#1abc9c',
+          tabBarInactiveTintColor: 'white',
+          tabBarActiveBackgroundColor: '#16a085',
+          tabBarActiveTintColor: 'white',
+          sceneStyle: {backgroundColor: '#1abc9c'},
+          tabBarIcon: ({focused, color, size}) =>
+            setTabIcon(route.name, focused, color, size),
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="About" component={About} />
+        <Tab.Screen name="Contact" component={Contact} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
