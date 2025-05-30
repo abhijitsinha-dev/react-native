257eab52e3a8048520082360870efd8268aeb896
Abhijit Sinha <abhijitsinhaofficial@gmail.com>
2025-05-30 20:37:46 +0530

    AsyncStorage Complete

--- App.jsx
+++ App.jsx
@@ -1,32 +1,12 @@
+import {View, Text} from 'react-native';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
-import Home from './src/navigation/Home';
-import About from './src/navigation/About';
-import Contact from './src/navigation/Contact';
-import {Platform, StatusBar, Text, View} from 'react-native';
-
-const Tab = createMaterialTopTabNavigator();
+import AsyncStorageEx from './src/components/AsyncStorageEx';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          tabBarStyle: {
-            backgroundColor: '#1abc9c',
-            paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-          },
-          tabBarLabelStyle: {color: 'white'},
-          tabBarIndicatorStyle: {backgroundColor: '#34495e'},
-          sceneStyle: {backgroundColor: '#1abc9c'},
-        })}>
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="About" component={About} />
-        <Tab.Screen name="Contact" component={Contact} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={{flex: 1}}>
+      <AsyncStorageEx />
+    </View>
   );
 };
 
